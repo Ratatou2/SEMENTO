dd54fabe3b95b3fa4506975ec38d5ba0fff49946
최서현 <cheshire99.dev@gmail.com>
2024-04-18 13:50:38 +0900

    :memo: 프로젝트 기본 폴더구조 변경

---
+++ Simulation/PythonLogic/error_occur.py
@@ -0,0 +1,61 @@
+from enum import Enum
+from random import random
+
+# 에러 종류 ENUM
+class Error(Enum):
+    COMMUNICATION = "COMMUNICATION"
+    OHT = "OHT"
+    FACILITY = "FACILITY"
+
+# 에러 발생시키는 함수
+# 에러의 확률 순위 : 확률은 [통신에러] > [OHT에러] > [설비에러] 순으로 높다
+def error_occur(communication_prob=0.1, OHT_prob=0.1, facility_prob=0.1):
+    # 각 확률을 따로 계산
+    communication_random = random()
+    OHT_random = random()
+    facility_random = random()
+    delay = 0
+
+    # print("[통신] 에러 확률 : ", communication_random, " / ", communication_prob)
+    # print("[OHT] 에러 확률 : ", OHT_random, " / ", OHT_prob)
+    # print("[설비] 에러 확률 : ", facility_random, " / ", facility_prob)
+
+    if communication_random < communication_prob:
+        delay += error_delay("COMMUNICATION")
+        return delay
+    if OHT_random < OHT_prob:
+        delay += error_delay("OHT")
+        return delay
+    if facility_random < facility_prob:
+        delay += error_delay("FACILITY")
+        return delay
+
+    return delay
+
+# 에러 발생시 딜레이 발생
+def error_delay(cause):
+    # 각 에러에 대한 딜레이 (단위 : sec)
+    communication_delay = 2
+    OHT_delay = 5
+    facility_delay = 10
+
+    if cause == Error.COMMUNICATION.value:
+        print("[System] 통신 에러")
+        return communication_delay
+
+    elif cause == Error.OHT.value:
+        print("[System] OHT 에러")
+        return OHT_delay
+
+    elif cause == Error.FACILITY.value:
+        print("[System] 설비 에러")
+        return facility_delay
+
+    else:
+        print("알 수 없는 에러")
+
+
+if __name__ == '__main__':
+
+    # 함수 호출
+    print("지연 추가 시간 : ", error_occur())
---
+++ Simulation/PythonLogic/start_end_permutation.py
@@ -0,0 +1,59 @@
+import time
+from queue import Queue
+from itertools import permutations
+from random import choice
+
+
+OHT_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15',
+            '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30']
+
+load_list = ['36', '85', '14', '59', '72', '3', '68', '93', '50', '9',
+             '46', '88', '71', '99', '16', '29', '87', '30', '95', '61',
+             '1', '83', '7', '69', '42', '35', '74', '10', '94', '76',
+             '12', '5', '26', '38', '49', '77', '98', '43', '65', '90']
+
+wait_queue = Queue()  # 중복 제거를 위해 set
+
+# 화물 배송을 위한 get_point, drop_point 설정
+def generate_delivery_order(dock_list=None, pick=2):
+    # 따로 선착장 리스트가 주어지는게 아니라면, 디폴트 실행
+    if dock_list is None: dock_list = ['A', 'B', 'C', 'D']
+
+    # 순열 제작
+    perm = permutations(dock_list, pick)
+
+    # 순열에서 무작위로 하나를 선택
+    selected_permutation = choice(list(perm))
+
+    return selected_permutation
+
+
+# def find_way(get_point, drop_point):
+#     # 랜덤 start_point
+#     OHT_location = choice(load_list)
+#
+#     # start_point부터 get_point까지 길찾기
+#     # 함수(OHT_location, get_point),
+#
+#     # get_point부터 end_point까지 길찾기
+#     # 함수(get_point, end_point)
+#
+#     return OHT_location, get_point, drop_point
+
+
+if __name__ == '__main__':
+    # 초기 작업 대상 OHT, queue 세팅
+    for OHT in OHT_list: wait_queue.put(OHT)
+
+    while (True):
+        if not wait_queue.empty():
+            current_OHT = wait_queue.get()
+            get_point, drop_point = generate_delivery_order()
+            print("[길찾기 Info]", current_OHT, "호기", find_way(get_point, drop_point))
+            print("큐의 내용물:", end=" ")
+            contents = [str(item) for item in wait_queue.queue]
+            print(", ".join(contents))
+            print("=======================")
+        else: print("Queue 대기중")
+        time.sleep(1)  # 1초 대기
+
