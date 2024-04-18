de5ef2fb966364ee4e3eb1d49bf01754bf39e7cd
최서현 <cheshire99.dev@gmail.com>
2024-04-18 13:52:45 +0900

    :tada: SpringBoot 프로젝트 시작!

---
+++ Backend/src/main/java/com/dfg/semento/SementoApplication.java
@@ -0,0 +1,13 @@
+package com.dfg.semento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SementoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SementoApplication.class, args);
+	}
+
+}
