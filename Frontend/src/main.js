692806d81379145f9b248b694de9b7aa6aa4bb5e
김수혁 <tngur1101@naver.com>
2024-04-30 09:51:34 +0900

    :sparkles: SideBar 생성
    사이드바를 생성하였습니다.

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,14 +1,20 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// fontawesome import
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas);
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
