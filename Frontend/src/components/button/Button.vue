df404851d0b4164241e07ab85e17b79170e9209d
최서현 <cheshire99.dev@gmail.com>
2024-05-01 19:25:49 +0900

    :lipstick: Button 컴포넌트 구현

--- Frontend/src/components/button/Button.vue
+++ Frontend/src/components/button/Button.vue
@@ -1,5 +1,48 @@
-<script setup></script>
+<!-- 
+** Button 사용법 **
 
-<template></template>
+ <Button
+ title="버튼제목" 
+ backgroundColor="black"
+ fontColor="white"/>
 
-<style scoped></style>
+-->
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Button Title",
+  },
+  backgroundColor: {
+    type: String,
+    default: "black",
+  },
+  fontColor: {
+    type: String,
+    default: "white",
+  },
+});
+</script>
+
+<template>
+  <button
+    :style="{ backgroundColor: backgroundColor, color: fontColor }"
+    class="button"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  height: 44px;
+  padding: 0px 9px;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 15px;
+}
+
+.button:hover {
+  cursor: pointer;
+}
+</style>
