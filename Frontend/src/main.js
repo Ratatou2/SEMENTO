ff4f0044db58903d41a21fb78a7d4a3f65f6833b
최서현 <cheshire99.dev@gmail.com>
2024-05-01 19:29:02 +0900

    :heavy_plus_sign: 날짜, selectBox 관련 의존성 추가

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -6,6 +6,13 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+//Vue Select
+import Multiselect from "vue-multiselect";
+
+// VCalendar import
+import VCalendar from "v-calendar";
+import "v-calendar/style.css";
+
 // fontawesome import
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -13,8 +20,15 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 library.add(fas);
 
 const app = createApp(App);
+
+//라이브러리 등록
+app.component("vue-multiselect", Multiselect);
 app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(VCalendar, {});
+
+//모듈 등록
 app.use(createPinia());
 app.use(router);
 
+//연결
 app.mount("#app");
