82fd9ea4cd396ead61e716c14114cad26ba168a0
곽미래 <alfo9490@naver.com>
2024-05-02 12:31:55 +0900

    :sparkles: 도넛 차트 UI 구현

--- Frontend/src/views/dashboard/components/job-analysis/DoughnutChart.vue
+++ Frontend/src/views/dashboard/components/job-analysis/DoughnutChart.vue
@@ -1,5 +1,75 @@
-<script setup></script>
+<script setup>
+import { reactive, onMounted } from 'vue';
+import { Chart, registerables } from 'chart.js';
+import { Doughnut } from 'vue-chartjs';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-<template></template>
+const data = [10, 30, 60];
+
+Chart.register(...registerables);
+Chart.register(ChartDataLabels);
+
+const chartData = reactive({
+  labels: ['Scheduling Error', 'OHT Error', 'Facility Error'],
+  datasets: [
+    {
+      backgroundColor: ['#BCE0F2', '#59A7FF', '#292D30'],
+      data,
+      borderWidth: 0
+    }
+  ]
+}); 
+
+const chartOptions = reactive({
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+        tooltip: {
+            backgroundColor: '#ffffff',
+            xAlign: 'center',
+            yAlign: 'center',
+            titleColor: '#555555',
+            titleAlign: 'center',
+            titleFont: {
+                weight: 'bold',
+                size: 20,
+                lineHeight: 1.5
+            },
+            bodyColor: '#555555',
+            bodyAlign: 'center',
+            bodyFont: {
+                size: 15,
+                lineHeight: 1.5
+            },
+            displayColors: false, // 색상 제거
+        },
+        datalabels: {
+            color: '#ffffff',
+            formatter: function(value, context) {
+                return value+'%';
+            },
+        },
+        legend: {
+            labels: {
+                usePointStyle: true,
+                boxHeight: 6
+            }
+        },
+    }, 
+})
+
+// Chart.js가 반응형 데이터를 제대로 처리하도록 setup
+onMounted(() => {
+  chartData.datasets.forEach(dataset => {
+    dataset.data = [...dataset.data]; // 데이터 배열을 새로운 배열로 교체
+  });
+});
+</script>
+
+<template>
+    <div>
+        <Doughnut :data="chartData" :options="chartOptions" />
+    </div>
+</template>
 
 <style scoped></style>
