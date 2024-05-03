c536efddbbb3f1779c22dbe05c49e1fb4d99c0da
김수혁 <tngur1101@naver.com>
2024-05-03 10:00:51 +0900

    :sparkles: 화이트카드 및 인포 생성
    인포 수정 필요
    화이트카드 수정 필요

--- Frontend/src/views/dashboard/DashBoardView.vue
+++ Frontend/src/views/dashboard/DashBoardView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import BlackDataCard from "@/components/card/BlackDataCard.vue";
 import Table from "@/components/table/Table.vue";
+import Info from "@/components/info/Info.vue";
+import WhiteCard from "./components/state-analysis/WhiteCard.vue";
 </script>
 <template>
   <div>Lorem</div>
@@ -17,6 +19,12 @@ import Table from "@/components/table/Table.vue";
       ['1', '1923', '300', '23회'],
       ['2', '1932', '300', '2회'],
     ]"
+  />
+  <Info />
+  <WhiteCard
+    title="작업이 가장 많은 시간대"
+    startTime="14:00"
+    endTime="16:00"
   />
 </template>
 <style scoped></style>
