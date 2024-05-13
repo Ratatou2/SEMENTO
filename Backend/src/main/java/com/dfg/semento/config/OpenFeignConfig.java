dcce3ef935d66640feb9c85994df964f472f41b8
최서현 <cheshire99.dev@gmail.com>
2024-05-13 10:03:56 +0900

    :heavy_plus_sign: open-feign-client 의존성 추가

---
+++ Backend/src/main/java/com/dfg/semento/config/OpenFeignConfig.java
@@ -0,0 +1,15 @@
+package com.dfg.semento.config;
+
+import feign.Logger;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients("com.dfg.semento")
+public class OpenFeignConfig {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
