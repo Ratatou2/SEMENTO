fea237e746680889c1feaf2fc14299ab4d63e20b
곽미래 <alfo9490@naver.com>
2024-05-21 14:14:57 +0900

    :ambulance: 알림 자동 꺼짐 삭제

--- Frontend/src/stores/notification.js
+++ Frontend/src/stores/notification.js
@@ -9,9 +9,9 @@ export const useNotificationStore = defineStore("notificationStore", () => {
   //플로팅 알림 띄우기
   const sendNotification = async () => {
     showNotification.value = true;
-    setTimeout(() => {
-      closeNotification();
-    }, 5000); // 5초 후에 알림을 닫음 (조절 가능)
+    // setTimeout(() => {
+    //   closeNotification();
+    // }, 5000); // 5초 후에 알림을 닫음 (조절 가능)
   };
 
   //플로팅 알림 닫기
